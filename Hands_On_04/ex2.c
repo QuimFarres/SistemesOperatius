3823a9e5b227fda336f60bc0d440bda6a4001532
QuimFarres <qfarres54@gmail.com>
2021-10-20 18:00:57 +0200

    Ejercicio 2 finalizado

---
+++ Hands_On_04/ex2.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+int main () {
+        pid_t p1 = fork();
+        assert (p1 >= 0);
+        if(p1 == 0)
+        {
+            printf("Hola sóc el procés fill amb pid %d \n", getpid());
+        }else
+        if(p1 > 0)
+        {
+            printf("Hola sóc el procés pare amb pid %d \n", getpid());
+        }
+
+        
+}
