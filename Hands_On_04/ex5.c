7ce2d22563a52a20efe48570b4d1db0ee8520227
JordiMateoUdL <jordi.mateo@udl.cat>
2021-10-27 10:29:58 +0200

    Comentaris ex5

--- Hands_On_04/ex5.c
+++ Hands_On_04/ex5.c
@@ -16,8 +16,8 @@ int RandomNumberGenerator(const int nMin, const int nMax)
 {
   int nRandonNumber = 0;
     srand(getpid());
-    nRandonNumber = rand()%(nMax-nMin) + nMin;
-
+    // Vigileu us falten () sino la fòrmula del mòdul no es correcta
+    nRandonNumber = rand()%((nMax-nMin) + nMin);
     return nRandonNumber;
 }
 
@@ -28,27 +28,25 @@ int main(){
     val = fork();
 
 
-    
     if(val==0){
         int a, b,exi, mer;
         printf("Hola sóc en Ned fill del matrimoni del Richard i la Lyarra amb pid = %d \n",getpid());
         val = fork();
-        b = RandomNumberGenerator(1,2);
-        a = wait(&exi);
-        mer = merge(WEXITSTATUS(exi), b);
         
-
-
- 
-
+        // b = RandomNumberGenerator(1,2); // Si voleu deixar aquesta línia eliminar la 41 i parell i fill tenen b assignada el seu valor, en la 44 exit(b)
+        // a = wait(&exi); // Aquest wait en aquest punt no té sentit, en el net retornarà -1
         if(val==0){
-
             printf("Hola sóc en Robb Stark amb pid = %d, sóc un fill del matrimoni de la Catelyn Stark i Ned Stark.\n" ,getpid());
             int random0 = RandomNumberGenerator(1,2);
+            printf("Soc en Robb amb pid = %d i he estat %s.\n",getpid(),(random0 == 0)? "decapitat": "apunyalat");
 
             exit(random0);
         }
-
+        
+        b = RandomNumberGenerator(1,2); // En aquest punt únicament ho farà el pare
+        a = wait(&exi); // Aquest wait únicament el farà el procés fill (pare en aquest cas)
+        mer = merge(WEXITSTATUS(exi), b);
+        printf("Soc en Ned amb pid = %d i he estat %s.\n",getpid(),(b == 0)? "decapitat": "apunyalat");
         exit(mer);
 
     }
